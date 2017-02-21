d41e3ebe46b051c43b7e55d6d919af2640df7064
Vadim Toptunov <umnijtony@mail.ru>
2017-02-21 11:10:36 +0300

    Add comments

--- War_and_Peace_monkeys-simple_script.py
+++ War_and_Peace_monkeys-simple_script.py
@@ -6,25 +6,30 @@ import string
 
 
 def lowercase():
+	# Create a random lowercase letters string
 	lower = ''.join(random.choice(string.ascii_lowercase) for x in range (8, 1024))
 	return lower
 
 
 def uppercase():
+	# Create a random uppercase letters string
 	upper = ''.join(random.choice(string.ascii_uppercase) for x in range(8, 1024))
 	return upper
 
 
 def digits():
+	# Create a random digits string
 	digs = ''.join(random.choice(string.digits) for x in range(8, 1024))
 	return digs
 
 
 def wildcards():
+	# Create a random wildcards string
 	wild = ''.join(random.choice(string.punctuation) for x in range(8, 1024))
 	return wild
 
 def result_function():
+	# Create a complicated password containing uppercase, lowercase letters, wildcards and digits.
     result = ''.join(random.choice(wildcards() + uppercase() + lowercase() + digits()) for x in range(8, 16))
     print "Your password is: " + result
 
